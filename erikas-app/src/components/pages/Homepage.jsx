977f04c30113346cce0a3621195a4d37e18a1864
bazzomir <blagoj.jovanovski@outlook.com>
2024-08-01 05:17:45 +0200

    services is started

--- erikas-app/src/components/pages/Homepage.jsx
+++ erikas-app/src/components/pages/Homepage.jsx
@@ -7,12 +7,12 @@ export default function Homepage() {
             <div className="row">
                 <div className="col-7">
                     <div className="d-flex row justify-content-start gap-3">
-                        <p className="col-12 first-text">WELCOME TO OUR WEBSITE</p>
+                        <p className="col-12 first-text text-uppercase">Welcome to our website</p>
                         <h1 className="col-12 second-text">
                             Shape Your Future <br />  with Global Innovation
                         </h1>
                         <p className="third-text">Discover comprehensive talent, where every step forward is tailored to your vision.</p>
-                        <button className="btn btn-purple">START TODAY</button>
+                        <button className="btn btn-purple text-uppercase">Start now</button>
                     </div>
                 </div>
                 <div className="col-5">
---
+++ erikas-app/src/components/pages/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Services() {
+    return (
+        <div className="services container mt-5">
+            <div className="row">
+                <h2 className="services--header text-center">Our Services</h2>
+                <p className="services--paragraph text-center">Welcome to our website</p>
+            </div>
+        </div>
+    )
+}
