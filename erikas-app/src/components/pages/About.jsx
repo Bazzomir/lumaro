786ff2c42cb78bd21dfb4af14fc4dc43eac28880
bazzomir <blagoj.jovanovski@outlook.com>
2024-08-31 01:22:06 +0200

    text-responsive

--- erikas-app/src/components/pages/About.jsx
+++ erikas-app/src/components/pages/About.jsx
@@ -35,8 +35,8 @@ export default function About() {
     return (
         <div className="about container-fluid mt-5">
             <div className="row justify-content-center align-items-center">
-                <div className="about-text col-12 col-lg-6">
-                    <p className="mb-0 mx-auto text-justify text-wrap w-75">
+                <div className="col-12 col-lg-6 ">
+                    <p className="mb-0 mx-0 mx-sm-auto text-center text-md-start about-text w-75">
                         <span className="text-purple">We're</span> Your Strategic Ally In The <span className="text-purple">Digital</span> IT World.
                     </p>
                 </div>
--- erikas-app/src/components/pages/Homepage.jsx
+++ erikas-app/src/components/pages/Homepage.jsx
@@ -7,13 +7,16 @@ export default function Homepage() {
         <div className="homepage container mt-5">
             <div className="d-flex row justify-content-between">
                 <div className="col-12 col-lg-5">
-                    <div className="d-flex row gap-5">
-                        <h1 className="col-12 m-0 second-text">
-                            Shape Your Future <br />  with Global Innovation
-                        </h1>
-                        <p className="col-12 m-0 third-text">Discover comprehensive talent, where every step forward is tailored to your vision.</p>
-                        {/* <button className="btn btn-purple text-uppercase">Start now</button> */}
-                        <Button btnName="Start Now"/>
+                    <div className="d-flex row gap-3 gap-md-5 text-center text-md-start">
+                        <div className="col-12">
+                            <h1 className="m-0 homepage-bigText">
+                                Shape Your Future <br />  with Global Innovation
+                            </h1>
+                        </div>
+                        <div className="col-12">
+                            <p className="m-0 homepage-smallText ">Discover comprehensive talent, where every step forward is tailored to your vision.</p>
+                        </div>
+                        <Button btnName="Start Now" />
                     </div>
                 </div>
                 <div className="col-0 col-lg-5 d-none d-lg-block">
