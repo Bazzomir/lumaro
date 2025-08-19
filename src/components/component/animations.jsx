1d48196d4c32ac5378ac7260cf5475d5e33ce298
bazzomir <bazzomir97@gmail.com>
2025-08-19 06:46:55 +0200

    added new animations

--- src/components/component/animations.jsx
+++ src/components/component/animations.jsx
@@ -1,6 +1,8 @@
 import Lottie from 'lottie-react';
 import LoadingBar from '../../assets/animation/LoadingBar.json';
 import scrollDown from '../../assets/animation/scrollDown.json';
+import CreativeTeam from '../../assets/animation/CreativeTeam..json';
+import BusinessPartners from '../../assets/animation/BusinessPartners.json'
 
 export function LoadingAnimation() {
     return (
@@ -21,5 +23,27 @@ export function ScrollDownAnimation({ onClick }) {
             className="scrollDownAnimation fixed-bottom mx-auto cursor-pointer"
             onClick={onClick}
         />
+    );
+};
+
+export function TeamAnimation() {
+    return (
+        <div className="TeamAnimation" data-aos="fade-left">
+            <Lottie
+                animationData={CreativeTeam}
+                loop={true}
+            />
+        </div>
+    );
+};
+
+export function PartnersAnimation() {
+    return (
+        <div className="PartnersAnimation" data-aos="fade-left">
+            <Lottie
+                animationData={BusinessPartners}
+                loop={true}
+            />
+        </div>
     );
 };
