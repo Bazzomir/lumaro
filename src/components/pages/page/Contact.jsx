1fd0c3bed503af1f1319386c771d49c1127cc981
bazzomir <bazzomir97@gmail.com>
2025-08-22 05:47:19 +0200

    ...

--- src/components/pages/page/Contact.jsx
+++ src/components/pages/page/Contact.jsx
@@ -34,46 +34,50 @@ export default function Contact() {
     }, []);
 
     return (
-        <>
-            <section className="contact container-fluid my-5 pt-5 pb-6 px-120 h-100 box-sizing overflow-hidden relative" id="contact">
-                <div className="row justify-content-center align-items-center">
-                    <div className=" col-12 col-lg-6">
-                        <div className="header-text--big">
-                                <span className="text-purple">Your</span> Success <br /> Is Our <span className="text-purple">Success</span>
-                            </p>
-                        </div>
-                        <Form />
-                    </div>
-                    <div className="col-0 col-lg-6 d-none d-lg-block relative">
-                        <img src="/lumaro/rectangle.svg" alt="Background Image" className="bg-triangle" />
+        // <>
+        <section className="contact container-fluid my-5 pt-5 pb-6 px-120 h-100 box-sizing overflow-hidden relative" id="contact">
+            <div className="row justify-content-center align-items-center">
+                <div className=" col-12 col-lg-6">
+                    <h2 className="mb-0 mx-0 mx-sm-auto text-center text-md-start header-text--big w-100 w-lg-75" data-aos="fade-right">
+                        <span className="text-purple">Your</span> Success <br /> Is Our <span className="text-purple">Success</span>
+                    </h2>
+                </div>
+                <Form />
+            </div>
+            <div className="col-0 col-lg-6 relative">
+                <img src="/lumaro/rectangle.svg" alt="Background Image" className="bg-triangle" />
 
-                        {/* <iframe width="800" height="600" src="https://www.youtube-nocookie.com/embed/XKe5cV1pvKw?si=f-zo2z_e2yND6hby" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen className="contact-video rounded" data-aos="fade-left" /> */}
-                        <PartnersAnimation />
+                {/* <iframe width="800" height="600" src="https://www.youtube-nocookie.com/embed/XKe5cV1pvKw?si=f-zo2z_e2yND6hby" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen className="contact-video rounded" data-aos="fade-left" /> */}
+                <PartnersAnimation />
 
+            </div>
+            {/* </div> */}
+            <div className="row justify-content-center align-items-center pt-5">
+                <div className="col-12 mt-3">
+                    <h3 className="text-center p-3 mb-0 header-text--small" data-aos="fade-down">
+                        <span className="text-purple">In</span> Their Own<span className="text-purple">Words</span>
+                    </h3>
+                </div>
+                <div className="col-12 pt-4">
+                    <div className="row p-0 m-0 gap-5 gap-md-4 gap-lg-0">
+                        {contactData.map((contact, i) => {
+                            const AvatarPath = avatarMap[contact.avatar];
+                            return (
+                                <div key={i} className="col-md-6 col-lg-4 my-4">
+                                    {AvatarPath && (
+                                        <ContactCard
+                                            avatarCard={AvatarPath}
+                                            fullName={contact.fullName}
+                                            quote={contact.quote}
+                                        />
+                                    )}
+                                </div>
+                            );
+                        })}
                     </div>
                 </div>
-                <div className="row justify-content-center align-items-center pt-5">
-                    <div className="col-12 mt-3">
-                    </div>
-                    <div className="col-12 pt-4">
-                        <div className="row p-0 m-0 gap-5 gap-md-4 gap-lg-0">
-                            {contactData.map((contact, i) => {
-                                const AvatarPath = avatarMap[contact.avatar];
-                                return (
-                                        {AvatarPath && (
-                                            <ContactCard
-                                                avatarCard={AvatarPath}
-                                                fullName={contact.fullName}
-                                                quote={contact.quote}
-                                            />
-                                        )}
-                                    </div>
-                                );
-                            })}
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
+            </div>
+        </section >
+        // </>
     );
 }
