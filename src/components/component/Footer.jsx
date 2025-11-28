84651fb0577149f88d9f21d579bb686134ac4c37
bazzomir <bazzomir97@gmail.com>
2025-11-28 04:53:22 +0100

    deleted commented code

--- src/components/component/Footer.jsx
+++ src/components/component/Footer.jsx
@@ -17,19 +17,15 @@ export const Footer = ({ activeLink, onNavClick }) => {
             <div className="py-5 px-120 mt-5">
                 <div className="row mt-5">
                     <div className="col-sm-12 col-lg-5">
-                        {/* <h2 className="footer-logo">Lumaro</h2> */}
                         <h2 className="footer-logo">
-                            {/* {footetData.header.title} */}
                             <HighlightedText text={footetData.header.title} />
                         </h2>
                         <div className="footer-paragraph">
                             <p className="footer-paragraph__text py-4 mb-0">
-                                {/* Empowering businesses with cutting-edge solutions, Lumaro specializes in connecting you with global talent and innovative strategies for sustainable growth. */}
                                 {footetData.header.paragraph}
                             </p>
                         </div>
                         <div className="footer-paragraph__text pt-5">
-                            {/* 1776 Constitution Ave NW, Washington DC 20006 */}
                             {footetData.getInTouch.address}
                         </div>
                     </div>
@@ -76,43 +72,36 @@ export const Footer = ({ activeLink, onNavClick }) => {
                     </div>
                     <div className="col-sm-4 col-lg-3 pt-5 pt-lg-0">
                         <span className="footer-item-names">
-                            {/* Get In Touch */}
                             {footetData.getInTouch.title}
                         </span>
                         <ul className="footer-getInTouch mt-3">
                             <li className="getInTocuh-item">
                                 <Link className="getInTocuh-link" to="mailto:contact@lumaro.com">
-                                    {/* Email: contact@lumaro.com */}
                                     Email: {footetData.getInTouch.email}
                                 </Link>
                             </li>
                             <li className="getInTocuh-item">
                                 <Link className="getInTocuh-link" to="tel:+18883218572">
-                                    {/* Phone: +1 8883218572 */}
                                     Phone: {footetData.getInTouch.phone}
                                 </Link>
                             </li>
                             <li className="getInTocuh-item">
-                                {/* Hours: Mon-Fri 9:00AM - 5:00PM */}
                                 Hours: {footetData.getInTouch.hours}
                             </li>
                         </ul>
                     </div>
                     <div className="col-sm-4 col-lg-2 pt-5 pt-lg-0">
                         <span className="footer-item-names">
-                            {/* Social */}
                             {footetData.socialMedia[0]}
                         </span>
                         <ul className="footer-social mt-3">
                             <li className="social-item">
                                 <Link className="social-link" to="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
-                                    {/* LinkedIn */}
                                     {footetData.socialMedia[1]}
                                 </Link>
                             </li>
                             <li className="social-item">
                                 <Link className="social-link" to="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-                                    {/* Instagram */}
                                     {footetData.socialMedia[2]}
                                 </Link>
                             </li>
@@ -125,7 +114,7 @@ export const Footer = ({ activeLink, onNavClick }) => {
                     <div className="col-12 col-lg-5 px-0 footer-paragraph__text">©2025 Lumaro</div>
                     <div className="col-6 col-lg-2 pt-2 pt-lg-0 footer-paragraph__text">Terms & Conditions</div>
                     <div className="col-6 col-lg-2 pt-2 pt-lg-0 footer-paragraph__text">Privacy Policy</div>
-                    <div className="col-12 col-lg-3 footer-paragraph__text">Dev with 💜 by Bazzomir</div>
+                    <div className="col-12 col-lg-3 footer-paragraph__text">Made with 💜 by Bazzomir</div>
                 </div>
             </div>
         </div>
